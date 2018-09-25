2ff851e3262ad330c866448f19c456b5b6f2704d
Jorge Porras <jporrasv3011@gmail.com>
2018-09-25 14:58:53 -0500

    conexion

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+var amqp = require('amqplib/callback_api');
+var consumer = process.env.RABBITMQ_PORT_5672_TCP_ADDR || "amqp://localhost";
 
+amqp.connect(consumer, (err, conn)=>{
+    conn.createChannel(function(err, ch){
+
+        ch.assertQueue(q, {durable: false});
+        console.log('[*] Mensajes: %s.', q);
+
+        ch.consume(q, function(msg){
+            console.log("[x] %s", msg.content.toString());
+        }, {noAck:true});
+    });
+})
